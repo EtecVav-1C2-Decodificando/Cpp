dd9d254bed08ae4ff69bc80f39bec2bf588ed2e6
brayanfn <brayanekauan1301@gmail.com>
2025-08-14 17:12:36 -0300

    Add files via upload
    Adicionar loop para escolher continuar ou nao, alterar a logica da funcao que retorna o tipo da ordem

--- Projeto7/main.cpp
+++ Projeto7/main.cpp
@@ -1,35 +1,62 @@
 #include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int verificarSequencia(int num[]) {
+    bool crescente = true; 
+    bool decrescente = true;
+    
+    // Loop para verificar os números consecutivos
+    for (int i = 0; i < 4; i++) {
+        if (num[i + 1] < num[i]) {  // Se o próximo número for menor, não é crescente
+            crescente = false;
+        }
+        if (num[i + 1] > num[i]) {  // Se o próximo número for maior, não é decrescente
+            decrescente = false;
+        }
+    }
+    
+    // Retornar o tipo da ordem
+	if (crescente) {
+	    return 1;  // Se for crescente, retorna 1 e sai da função
+	} else if (decrescente) {
+	    return -1;  // Se não for crescente, mas for decrescente, retorna -1
+	} else {
+	    return 0;  // Caso contrário, retorna 0 (sequência não ordenada)
+	} 
+}
 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int main(int argc, char** argv) {
 	int num[5];
-	bool crescente= true;
-	bool decrescente= true;
+	char r='s';
 	
-	std::cout << "Digite 5 numeros na entrada\n";
+	while(r=='s'){
 	
-	for (int i = 0; i < 5; i++) {
-        std::cout << "Digite o " << i + 1 << " numero: ";
-        std::cin >> num[i];
-    }
-        
-    for (int i = 0; i < 4; i++) {
-    
-	    if (num[i + 1] < num[i]) {
-        crescente = false;
-    	}
-    	if (num[i + 1] > num[i]) {
-        decrescente = false;
-    	}  
-    }
-    
-    if (crescente){
-        std::cout << "Essa e uma ordem crescente\n";
-    }else if(decrescente){
-        std::cout << "Essa e uma ordem decrescente\n";
-    }else{
-        std::cout << "Essa e uma sequencia nao ordenada\n";
+		std::cout << "Digite 5 numeros na entrada\n";
+		
+		for (int i = 0; i < 5; i++) { // Loop para ler os 5 numeros dentro do vetor
+	        std::cout << "Digite o " << i + 1 << " numero: ";
+	        std::cin >> num[i];
+	    }
+	    
+	    int tipo = verificarSequencia(num) ; // Executa a funcao para descobrir o tipo da ordem
+	    
+	    if (tipo == 1) { // Exibe o tipo da ordem de acordo com o resultado da funcao
+	    	
+	        std::cout << "Essa e uma ordem crescente\n";
+	        
+	    } else if (tipo == -1) {
+	    	
+	        std::cout << "Essa e uma ordem decrescente\n";
+	        
+	    } else {
+	    	
+	        std::cout << "Essa e uma sequencia nao ordenada\n";
+	        
+	    }
+	    
+	    std::cout << "Continuar? s/n: ";
+	    std::cin >> r;
+	    
 	}
 	
 	return 0;
