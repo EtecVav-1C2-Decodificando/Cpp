b2ff277219f5aff5a4456df2207a80e08ffd4c2f
brayanfn <brayanekauan1301@gmail.com>
2025-08-14 17:50:59 -0300

    Add files via upload
    Adicionar loop para escolher continuar ou nao, alterar a logica da funcao que retorna a soma dos numeros impares ate o numero escolhido

--- Projeto1/main.cpp
+++ Projeto1/main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
-	
-	using namespace std;
-	
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-	bool ehImpar (int num){
-		return num % 2 !=0;
-	}
-
-int main(int argc, char** argv) {
-	int num, impar=0;
-
-	cout<<"Digite um numero inteiro: ";
-	cin>>num;
-	
-	for (int i=1; i <= num; i++){
-		if(ehImpar(i)){
-			impar=impar+i;
-		}
-	}
-	
-	cout<<"A soma dos numeros"<<impar;
-	
-	return 0;
+// Função retorna a soma dos números ímpares até o número
+int ehImpar(int num) {
+    int soma = 0;  // Variável para armazenar a soma dos ímpares
+    
+    for (int i = 1; i <= num; i++) {
+        if (i % 2 != 0) {  // Verifica se 'i (numero atual do loop)' é ímpar
+            soma = soma+i;  // Adiciona 'i' à soma
+        }
+    }
+    
+    return soma;  // Retorna a soma dos números ímpares
 }
+
+int main() {
+	char r='s';
+    int num;
+    
+    while(r=='s'){
+
+    std::cout << "Digite um numero inteiro: ";
+    std::cin >> num;
+
+    // executa a função e guarda o resultado na variável 
+    int soma = ehImpar(num);
+
+    // Exibe a soma dos números ímpares
+    std::cout << "A soma dos numeros impares ate o numero escolhido e: " << soma;
+    
+    std::cout << "\nContinuar? s/n: ";
+    std::cin >> r;
+    
+	}
+
+    return 0;
+}
+
