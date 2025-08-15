1c3399e4b06921e21bb2e9179585750ee9b1f059
WilliamLucas835 <Williamlucasd04@gmail.com>
2025-08-15 18:13:40 -0300

    Add files via upload
    Rascunho inicial do código 5.

---
+++ Projeto 5/main.cpp
@@ -0,0 +1,39 @@
+##include <cstdlib>
+#include <iostream>
+#include <string>
+
+
+
+bool e_palindromo(const std::string& palavra) {
+    int inicial = 0;
+    int final = palavra.length() - 1;
+        
+        while(inicial < final){
+               if (palavra [inicial] != palavra[final]) {
+                  return false; 
+               }
+               
+               inicial++;
+               final--;
+          }
+          
+          return true;
+
+
+int main(int argc, char *argv[])
+{
+    std:: string palavra;
+
+   
+    std:: cout << "Escreva uma palavra: ";
+    std:: getline (std:: cin, palavra);
+    
+    if(e_palindromo(palavra)){
+        std:: cout << "Esta palavra e'" << palavra << " 'um palindromo";
+    } else {
+           std:: cout << "Esta palavra e' " << palavra << " 'nao e um palindromo";
+    } 
+    
+    return 0;
+    
+}
