2ab4ad2022ae449947ff24568186510fbcd5930a
DanielKolde <dkolde7bfund2@gmail.com>
2025-08-26 16:00:38 -0300

    Add files via upload
    Projeto 4, página 29 e 30. Ler dois números inteiros e efetuar os seguintes cálculos: adição, subtração, multiplicação e divisão. Criar função para cada um dos cálculo. Exibir o resultado de todos cálculos.

---
+++ Projeto4/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int adicao (int a, int b) {
+	
+	int soma=a+b;
+	return soma;
+}
+
+int subtracao (int a, int b) {
+	
+	int menos=a-b;
+	return menos;
+}
+
+int multiplicacao (int a, int b) {
+	
+	int vezes=a*b;
+	return vezes;
+}
+
+float divisao (int a, int b) {
+	
+	float dividido= static_cast<float>(a) / b;
+	return dividido;
+
+}
+
+int main(int argc, char** argv) {
+	int a, b;
+	std:: cout<<"Escreva um numero: ";
+	std:: cin>>a;
+	
+	std:: cout<<"Escreva um numero: ";
+	std:: cin>>b;
+	
+	int soma = adicao (a, b);
+	std:: cout<<a<< " + "<<b<<" = "<<soma<<"\n";
+	
+	int menos = subtracao (a, b);
+	std:: cout<<a<< " - "<<b<<" = "<<menos<<"\n";
+	
+	int vezes = multiplicacao (a, b);
+	std:: cout<<a<< " x "<<b<<" = "<<vezes<<"\n";
+	
+	float dividido = divisao (a, b);
+	std:: cout<<a<< " / "<<b<<" = "<<dividido;
+	
+	return 0;
+}
