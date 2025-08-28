3ba7d05c8cd5404785ae4bf7fec7fcfd62bbcce9
WilliamLucas835 <Williamlucasd04@gmail.com>
2025-08-28 14:30:52 -0300

    Add files via upload
    Projeto 2, páginas 29/30 apostila Portugol.

---
+++ Projeto2/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int potencia (int base, int expoente){
+	 int resultado =1;
+	 for (int i = 1; i <= expoente; i++) {
+            resultado *= base;
+    }
+    return resultado;
+}
+
+
+int main(int argc, char** argv) {
+
+	char r = 's';
+	int base, expoente;
+	while(r == 's'){
+	
+	
+		std:: cout << "De um valor para base: ";
+		std:: cin >> base;
+		
+		std:: cout << "De um valor para o expoente: ";
+		std:: cin >> expoente;
+		
+		int resultado = potencia(base, expoente);
+    
+		std:: cout << "A potencia e: " << resultado;
+		std:: cout << "\nDeseja continuar (S/N)?";
+		std:: cin >> r; 
+	}
+		
+		
+		
+	return 0;
+}
+
