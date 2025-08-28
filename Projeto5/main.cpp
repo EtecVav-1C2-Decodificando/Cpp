f4e2155ec98438f6dcd25611c5519c9475348cce
WilliamLucas835 <Williamlucasd04@gmail.com>
2025-08-28 14:37:53 -0300

    Add files via upload

---
+++ Projeto5/main.cpp
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <stdbool.h>
+
+
+bool e_palindromo(const std::string& palavra) {
+    int inicial = 0;
+    int final = palavra.length() - 1;
+        
+        while(inicial < final){
+               if (palavra [inicial] != palavra[final]) {
+                  return false; 
+               }
+               
+               inicial++;
+               final--;
+          }
+        
+          return true;
+}
+
+int main(int argc, char *argv[])
+{
+    std:: string palavra;
+
+   
+    std:: cout << "Escreva uma palavra: ";
+    std:: getline (std:: cin, palavra);
+    
+    if(e_palindromo(palavra)){
+        std:: cout << "Esta palavra e um palindromo";
+    } else {
+           std:: cout << "Esta palavra nao e um palindromo";
+    } 
+    
+    return 0;
+    
+}
+
+
