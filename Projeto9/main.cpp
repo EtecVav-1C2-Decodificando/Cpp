72a62d0fe5ec54d136a1357a20ba6e1a2c0e86fe
brayanfn <brayanekauan1301@gmail.com>
2025-08-28 10:20:22 -0300

    Update main.cpp
    Indentação corrigida e comentarios adicionados para melhor compreensão

--- Projeto9/main.cpp
+++ Projeto9/main.cpp
@@ -1,14 +1,16 @@
-#include <iostream>
+ #include <iostream>
 #include <string>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 std::string substituiVogal (std::string palavra) {
+	//Percorre todas as letras da palavra verificando vogais.
 	for (int i=0; i < palavra.length(); i++) {
 		char c = palavra[i];
+		//Troca a letra da posicao i por *, caso seja uma vogal
 		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
             c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
-        	palavra[i] = '*';
+        	palavra[i] = '*'; 
     	}
 	}
 	return palavra;
@@ -21,13 +23,14 @@ int main(int argc, char** argv) {
 	
 	while (r=='s'){
 	
-    std::cout << "Digite uma palavra: ";
-    std::cin >> palavra;
-
-    std::cout << "Resultado: " << substituiVogal(palavra) ;
-    
-    std::cout << "\nContinuar s/n?";
-    std::cin >> r;
+	    std::cout << "Digite uma palavra: ";
+	    std::cin >> palavra;
+	
+	    std::cout << "Resultado: " << substituiVogal(palavra) ; // Executa a funcao e exibe a palavra final.
+	    
+	    std::cout << "\nContinuar s/n?";
+	    std::cin >> r;
     }
     return 0;
 }
+
