41b98a5ba3b629f025fecd5c9376ce550219f24e
Gustavo de Lima Costa <gustavo.limacosta1804@gmail.com>
2025-08-28 01:44:00 -0300

    Update project3.cpp
    Correção de código

--- Projeto3/project3.cpp
+++ Projeto3/project3.cpp
@@ -2,37 +2,37 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+float pesoIdealMasculino(float altura) {
+    return (72.7 * altura) - 58;
+}
 
-	float altura, pesoideal;
-	char sexo, r;
-	r='s';
-	
-	while(r=='s'){
-	
-	std::cout<<"Qual seu sexo? (responda F para feminino e M para masculino) ";
-	std::cin>>sexo;
-	std::cout<<"Qual sua altura?";
-	std::cin>>altura;
-	
-	if(sexo=='M'){
-		pesoideal=(altura*72.7)-58;
-		
-	std::cout << "Seu peso ideal e de: " << pesoideal << std::endl;
-		
-	}
-	else{
-	if(sexo=='F'){
-			pesoideal=(altura*62.1)-44.7;
-			
-	std::cout << "Seu peso ideal e de: " << pesoideal << std::endl;
-		}
-	}
-	
-	std::cout<<"Continuar?(s/n)";
-	std::cin>>r;
-	
-	}
-	getchar();
-	return 0;
+float pesoIdealFeminino(float altura) {
+    return (62.1 * altura) - 44.7;
+}
+
+int main(int argc, char** argv) {
+    float altura, pesoideal;
+    char sexo, r = 's';
+    
+    while (r == 's' || r == 'S') {
+        std::cout << "Qual seu sexo? (F para feminino / M para masculino): ";
+        std::cin >> sexo;
+        std::cout << "Qual sua altura? ";
+        std::cin >> altura;
+        
+        if (sexo == 'M' || sexo == 'm') {
+            pesoideal = pesoIdealMasculino(altura);
+            std::cout << "Seu peso ideal e de: " << pesoideal << " kg" << std::endl;
+        } 
+        else if (sexo == 'F' || sexo == 'f') {
+            pesoideal = pesoIdealFeminino(altura);
+            std::cout << "Seu peso ideal e de: " << pesoideal << " kg" << std::endl;
+        } 
+        
+        
+        std::cout << "\nDeseja continuar? (s/n): ";
+        std::cin >> r;
+    }
+    
+    return 0;
 }
